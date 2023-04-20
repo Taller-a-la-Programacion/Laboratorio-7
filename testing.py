eb244eb40227eb7a8ff348df305eb8471840ec5e
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-20 09:37:35 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -18,14 +18,14 @@ def test_cortarMatriz_4():
     
 matriz = [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
 
-def test_cortarMatriz_1():
+def test_cortarMatrizv2_1():
     assert Laboratorio7.cortarMatriz_v2(matriz, 2, 3, 2, 2) == [ [16,20], [40,50] ]
     
-def test_cortarMatriz_1():
+def test_cortarMatrizv2_2():
     assert Laboratorio7.cortarMatriz_v2(matriz, 2, 0, 2, 2) == [ [4,8], [10,20] ]
     
-def test_cortarMatriz_1():
+def test_cortarMatrizv2_3():
     assert Laboratorio7.cortarMatriz_v2(matriz, 0,0,5,5) == [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
     
-def test_cortarMatriz_4():
+def test_cortarMatrizv2_4():
     assert  isinstance( str(Laboratorio7.cortarMatriz_v2(matriz, 2, 1, 6, 6), str) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
