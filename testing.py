1746697ac69604905e94fa212c74928e2339f1ba
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-21 08:29:11 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -16,38 +16,38 @@ def test_cortarMatriz_4():
     assert  isinstance( str(Laboratorio7.cortarMatriz(matriz, 8, 5)), str) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
     
 ###########################################################################################    
-matriz = [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
+matriz2 = [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
 
 def test_cortarMatrizv2_1():
-    assert Laboratorio7.cortarMatriz_v2(matriz, 2, 3, 2, 2) == [ [16,20], [40,50] ]
+    assert Laboratorio7.cortarMatriz_v2(matriz2, 2, 3, 2, 2) == [ [16,20], [40,50] ]
     
 def test_cortarMatrizv2_2():
-    assert Laboratorio7.cortarMatriz_v2(matriz, 2, 0, 2, 2) == [ [4,8], [10,20] ]
+    assert Laboratorio7.cortarMatriz_v2(matriz2, 2, 0, 2, 2) == [ [4,8], [10,20] ]
     
 def test_cortarMatrizv2_3():
-    assert Laboratorio7.cortarMatriz_v2(matriz, 0,0,5,5) == [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
+    assert Laboratorio7.cortarMatriz_v2(matriz2, 0,0,5,5) == [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
     
 def test_cortarMatrizv2_4():
-    assert  isinstance( str(Laboratorio7.cortarMatriz_v2(matriz, 2, 1, 6, 6), str) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
+    assert  isinstance( str(Laboratorio7.cortarMatriz_v2(matriz2, 2, 1, 6, 6), str)) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
 
 
 ###########################################################################################                       
-matriz = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
-matriz2 = [ [1,0], [0,0], [1,1], [1,0], [0,1], [1,1]]
+matriz3 = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
+matriz4 = [ [1,0], [0,0], [1,1], [1,0], [0,1], [1,1]]
 
 def test_convertirBinADec_1():
-    assert Laboratorio7.convertirBinADec(matriz) == [16, 31, 10]
+    assert Laboratorio7.convertirBinADec(matriz3) == [16, 31, 10]
                        
 def test_convertirBinADec_2():
-    assert Laboratorio7.convertirBinADec(matriz2) == [2,0,3,2,1,3]
+    assert Laboratorio7.convertirBinADec(matriz4) == [2,0,3,2,1,3]
                        
 
 ###########################################################################################                       
-matriz = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
-matriz2 = [ ['A',1,0], [0,'B',0], ['F','F',1], ['A','B',5], [2,'C',9]]
+matriz5 = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
+matriz6 = [ ['A',1,0], [0,'B',0], ['F','F',1], ['A','B',5], [2,'C',9]]
 
 def test_convertirHexADec_1():
-    assert Laboratorio7.convertirHexADec(matriz) == [65536, 69905, 4112]
+    assert Laboratorio7.convertirHexADec(matriz5) == [65536, 69905, 4112]
                        
 def test_convertirHexADec_2():
-    assert Laboratorio7.convertirHexADec(matriz2) == [2576, 176, 4081, 2741, 713]                       
+    assert Laboratorio7.convertirHexADec(matriz6) == [2576, 176, 4081, 2741, 713]                       
